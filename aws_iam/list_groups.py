7103c5d0129467cc8ae534404a9b771e39705134
Haydz <haydnit@gmail.com>
2023-05-23 18:13:31 -0400

    groups

---
+++ aws_iam/list_groups.py
@@ -0,0 +1,14 @@
+import boto3
+iam = boto3.client('iam')
+
+# List all IAM groups
+response = iam.list_groups()
+for group in response['Groups']:
+    print(group['GroupName'])
+
+
+
+# List all groups a user belongs to
+response = iam.list_groups_for_user(UserName='USERNAME_HERE')
+for group in response['Groups']:
+    print(group['GroupName'])
